917d937b4e072b9abfed947a0dd401d1dac2b437
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2024-06-05 22:55:11 +0400

    Activity 6 - Mouse Trails

--- sketch.js
+++ sketch.js
@@ -1,19 +1,27 @@
+var count = 1; 
+var c; 
 function setup() { 
-createCanvas(600, 400);
+c = createCanvas(600, 400); 
 background(255); 
-stroke(0,0,255); 
-noFill(); 
-strokeWeight(3); 
-translate(0, height/2); 
-beginShape(); 
-var noiseCount= 0;//starts with 0 
-//start from 0,it will loop depending on the given width,
-//then i is increased by 1
-for(var i= 0; i< width; i+= 1) { 
-var ranY= noise(noiseCount); //position for y
-vertex(i, ranY* 100);//point
-noiseCount+= 0.02; 
+noStroke(); 
 } 
-endShape();
-
+function draw() { 
+if (mouseIsPressed) { 
+  fill(224,109,6); 
+  rect(mouseX, mouseY, 30, 30); 
+  fill(255,197,58);
+  rect(mouseX,mouseY,20,20);
+   fill(240,153,32);
+  ellipse(mouseX,mouseY,10,10)
+} 
+} 
+function keyPressed() { 
+if (keyCode == 65) { 
+  saveCanvas(c, 'canvasName' + count, 'jpg'); 
+  count++; 
+} 
 }
+
+
+
+
