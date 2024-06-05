00228af5bde1ad60e0bfb2632dea2c17c7d5a431
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2024-06-05 22:27:48 +0400

    Activity 4 - Simple Pattern(part 2)

--- sketch.js
+++ sketch.js
@@ -1,34 +1,26 @@
-var img, x, y;
-function preload() {
-img = loadImage("heir.jpg");
-}
+var maxCol=5;
+var maxRow=4;
+var circleD=60;
+
 function setup() {
-createCanvas (400,400);
-background(0);
-noStroke();
-
+  createCanvas(400, 400);
+  noStroke();
+  //colorMode(HSB,360,100,100);
+  background(0);
+  ellipseMode(CORNER);
+  var xSpacing=(width/maxCol);
+  var ySpacing=(height/maxRow);
+  translate(30,20);
+  for(var x=0;x<maxCol;x++){
+  for(var y=0;y<maxRow;y++){
+    if(x==1){
+    fill(255,0,171);
+      }else{
+        fill(141,16,195);
+      }
+  
+    ellipse(x*xSpacing,y*ySpacing,circleD,circleD)
+  
+  }
 }
-function draw() {
-background(0);
-x = mouseX;
-y = mouseY;
-image( img, 0, 0);
-var A = get(x, y);
-fill(A);
-rect (x,y,90,90);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
