9870b81feb48775ff326441b0d941456a924ec7c
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2024-06-05 22:29:33 +0400

    Activity 5  - Typography(part-1)

--- sketch.js
+++ sketch.js
@@ -1,19 +1,15 @@
-function setup() { 
-createCanvas(600, 400);
-background(255); 
-stroke(0,0,255); 
-noFill(); 
-strokeWeight(3); 
-translate(0, height/2); 
-beginShape(); 
-var noiseCount= 0;//starts with 0 
-//start from 0,it will loop depending on the given width,
-//then i is increased by 1
-for(var i= 0; i< width; i+= 1) { 
-var ranY= noise(noiseCount); //position for y
-vertex(i, ranY* 100);//point
-noiseCount+= 0.02; 
-} 
-endShape();
+var word="BSU "
+var font1;
+function preload(){
+font1=loadFont("BlackOpsOne-Regular.ttf");
 
+}
+function setup(){
+  createCanvas(800,500);
+  background(255,0,0);
+  fill(0,0,255);
+  
+  textFont(font1,100);
+  textAlign(CENTER);
+  text(word,width/2,height/2);
 }
