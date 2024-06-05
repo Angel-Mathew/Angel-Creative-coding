9e5114b4e6088ad6e7bf891631c3af33736d3582
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2024-06-05 22:50:54 +0400

    Activity 4 - Simple Pattern(part 2)

--- sketch.js
+++ sketch.js
@@ -1,19 +1,26 @@
-var circleSize=80;
+var maxCol=5;
+var maxRow=4;
+var circleD=60;
+
 function setup() {
   createCanvas(400, 400);
-  background(255);
-  translate(circleSize/2,circleSize/2);
-//for loop is considering the width and height of the canvas
-for(var x=0;x<width;x+=circleSize){
-  for(var y=0;y<height;y+=circleSize){
-    fill(1,1,1);
-    ellipse(x,y,circleSize*1.2,circleSize*1.2);
-    fill(255,0,0);
-    ellipse(x,y,circleSize*0.8,circleSize*0.8);
-    fill(253,8,131);
-    ellipse(x,y,circleSize*0.3,circleSize*0.3);  
+  noStroke();
+  //colorMode(HSB,360,100,100);
+  background(0);
+  ellipseMode(CORNER);
+  var xSpacing=(width/maxCol);
+  var ySpacing=(height/maxRow);
+  translate(30,20);
+  for(var x=0;x<maxCol;x++){
+  for(var y=0;y<maxRow;y++){
+    if(x==1){
+    fill(255,0,171);
+      }else{
+        fill(141,16,195);
+      }
+  
+    ellipse(x*xSpacing,y*ySpacing,circleD,circleD)
+  
   }
 }
 }
-
-
